96bf0e771b12d743797393b4ac6aac3c40d170e3
Nicolas Echeverry <nikoxx9900@gmail.com>
2022-05-25 17:55:49 -0500

    fix db credentials

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'laniakea'),
-      user: env('DATABASE_USERNAME', 'root'),
+      database: env('DATABASE_NAME', ''),
+      user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
